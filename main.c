d711948692c6470d7f138f1c57a84dd98e78f4b8
navyracooon <navyracooon@gmail.com>
2023-07-11 10:58:21 +0900

    Sup. 7

--- main.c
+++ main.c
@@ -24,8 +24,13 @@ int main() {
                &width, &height);
 
     // 処理層
-    int i = 0;
-    save_mnist_bmp(train_x + 784 * i, "train_%05d.bmp", i);
+    int sum = 0;
+    for (int i=0; i<test_count; i++) {
+        if (inference3(A_784x10, b_784x10, test_x + i * width * height) == test_y[i]) {
+            sum++;
+        }
+    }
+    printf("%f%%\n", sum * 100.0 / test_count);
     return 0;
 }
 
